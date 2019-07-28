c2ec4221f70cc63850ed88e92ef9fce38fa90e8d
Ludmila9 <dzubaludmila9@gmail.com>
2019-07-29 02:45:53 +0300

    change widht foter .contairer, del padding, margin

--- js/main.js
+++ js/main.js
@@ -28,13 +28,7 @@
         appendDots: '.news-slider__check',
     });
 
-var $page = $('html, body');
-$('a[href*="#"]').click(function() {
-    $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 400);
-    return false;
-});
+
 	
 })(jQuery); 
 	
